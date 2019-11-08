1e5fd4b95766d1874b83d407b8318864b5aea359
mayor01 <mayor01@users.noreply.github.com>
2019-11-08 15:36:34 +0100

    changes made

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -20,8 +20,8 @@ export class ProductService {
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response) =><Album> response.json());
   }
-
+  
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response) => <Product[]> response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
